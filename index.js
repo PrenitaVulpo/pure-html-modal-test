3e35c8ac4a65c3fa0e3e61af2a59ded52d5e62ea
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-26 14:17:34 -0300

    add JS

---
+++ index.js
@@ -0,0 +1,9 @@
+const callModal = () => {
+  const modal = document.getElementById("series-modal");
+  modal.classList.add("show-modal");
+};
+
+const hideModal = () => {
+  const modal = document.getElementById("series-modal");
+  modal.classList.remove("show-modal");
+};
